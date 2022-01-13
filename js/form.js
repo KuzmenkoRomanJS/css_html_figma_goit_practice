cf973e2794000fd9d4b09a0be9363f9f3d590061
Roman Kuzmenko <83546337+KuzmenkoRomanJS@users.noreply.github.com>
2022-01-13 18:57:14 +0200

    Update form.js
    Added querySelector("#slider") to make the switch look better

--- js/form.js
+++ js/form.js
@@ -1,52 +1,52 @@
-const MODAL_ACTIVE_CLASS = 'modal-active';
+const MODAL_ACTIVE_CLASS = "modal-active";
 
-const callFormBtn = document.querySelector('#call-form-btn');
+const callFormBtn = document.querySelector("#call-form-btn");
 
-const modalForm = document.querySelector('#modal-form');
-const modalSuccess = document.querySelector('#modal-success');
+const modalForm = document.querySelector("#modal-form");
+const modalSuccess = document.querySelector("#modal-success");
 
-const form = document.querySelector('#modal-form form');
+const form = document.querySelector("#modal-form form");
 
 function closeModals(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    modalForm.classList.remove(MODAL_ACTIVE_CLASS);
+  modalForm.classList.remove(MODAL_ACTIVE_CLASS);
   modalSuccess.classList.remove(MODAL_ACTIVE_CLASS);
-  
-  document.body.classList.remove('body-fixed')
+  document.querySelector("#slider").style.opacity = 1;
+
+  document.body.classList.remove("body-fixed");
 }
 
 function openSuccessModal() {
-    modalForm.classList.remove(MODAL_ACTIVE_CLASS);
+  modalForm.classList.remove(MODAL_ACTIVE_CLASS);
   modalSuccess.classList.add(MODAL_ACTIVE_CLASS);
-  
-  document.body.classList.add('body-fixed')
 
-    const modalFormClose = document.querySelector('#modal-success-close');
-  modalFormClose.addEventListener('click', closeModals);
-  
-};
+  document.body.classList.add("body-fixed");
 
+  const modalFormClose = document.querySelector("#modal-success-close");
+  modalFormClose.addEventListener("click", closeModals);
+
+  slider.style.display = "none";
+}
 
 function sendUserInfo(e) {
-  e.preventDefault()
+  e.preventDefault();
   const formData = new FormData(form);
 
-  fetch('/', {
-    method: 'POST',
+  fetch("/", {
+    method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
+    body: new URLSearchParams(formData).toString(),
   }).then(openSuccessModal);
 }
 
+callFormBtn.addEventListener("click", function () {
+  modalForm.classList.add(MODAL_ACTIVE_CLASS);
+  document.querySelector("#slider").style.opacity = 0.2;
+  const modalFormClose = document.querySelector("#modal-form-close");
+  modalFormClose.addEventListener("click", closeModals);
 
-callFormBtn.addEventListener('click', function () {
-    modalForm.classList.add(MODAL_ACTIVE_CLASS);
+  document.body.classList.add("body-fixed");
 
-    const modalFormClose = document.querySelector('#modal-form-close');
-  modalFormClose.addEventListener('click', closeModals);
-  
-  document.body.classList.add('body-fixed')
-
-    form.addEventListener('submit', sendUserInfo);
+  form.addEventListener("submit", sendUserInfo);
 });
